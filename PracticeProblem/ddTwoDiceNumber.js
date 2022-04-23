5c570476fcc1a83b5c8904293d9b9e182d681158
Resh@308 <reshsingh1995@gmail.com>
2022-04-23 12:10:42 +0530

    added two dice  random dice number

---
+++ PracticeProblem/ddTwoDiceNumber.js
@@ -0,0 +1,10 @@
+//Add two Random Dice Number and Print the Result
+
+let dice1 = Math.floor(Math.random() * 6) + 1;
+console.log("Dice number 1 is : " + dice1);
+
+let dice2 = Math.floor(Math.random() * 6) + 1;
+console.log("Dice number 2 is : " + dice2);
+
+let result = dice1 + dice2;
+console.log(result);
